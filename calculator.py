1d519b1955135c5128b765a1500d307bc9092f15
Philip Ruttley <philip.ruttley@ou.ac.uk>
2026-08-22 18:40:22 +0100

    Fix formatting and improve calculator function

--- calculator.py
+++ calculator.py
@@ -1,18 +1,23 @@
 def calculator ():
-print ("Simple Python Calcuator")
-print ("Operations:+, -, *, /, **, %")
-print ("Type 'q' to quit\n")
-while True:
-try:
-expression = input ("Enter expression (e.g. 12 + 5:"). strip ()
-if expression.lower() in ('q', 'quit', 'exit' ):
-print ("Goodbye!")
+  
+ print("Simple Python Calcuator")
+ print("Operations:+, -, *, /, **, %")
+ print("Type 'q' to quit\n")
+  
+ while True:
+ try:
+expression = input ("Enter expression (e.g. 12 + 5:").strip ()
+
+if expression.lower() in ('q', 'quit', 'exit'):
+print("Goodbye!")
 break
-result = eval (expression, {"__builtins__: {}},{})
-print=(f"Result:{result}\n")
+result = eval(expression, {"__builtins__:{}},{})
+print(f"Result:{result}\n")
+                           
 except ZeroDivisionError:
-print ("Error: Division by zero\n")
+print("Error: Division by zero\n")
 except Exception:
-print ("Invalid expression. Try again.\n")
-if__name__ = "__main__":
+print("Invalid expression. Try again.\n")
+                            
+if__name__=="__main__":
 calculator()
